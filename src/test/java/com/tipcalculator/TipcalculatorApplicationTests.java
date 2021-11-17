6b413c61bf7e406d5ba3215f1a1928612b6e934a
Tebogo Mashika <tebogo@gmail.com>
2021-11-17 22:55:58 +0200

    tip-calculator

---
+++ src/test/java/com/tipcalculator/TipcalculatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tipcalculator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TipcalculatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
